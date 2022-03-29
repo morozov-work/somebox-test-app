16e9f9d218517a68f830df464ae9320d7e522f55
unknown <morozov-work@outlook.com>
2022-03-29 20:50:21 +0300

    add row component

---
+++ src/components/Row.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row">
+        <slot />
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Row',
+}
+</script>
+
+<style lang="css">
+.row {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    column-gap: 40px;
+    align-items: center;
+    margin-bottom: 40px;
+}
+</style>
